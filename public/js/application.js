41738c48e65fc1b659ed46b62245d4ab22e2cc75
iainkirkpatrick & Jonathan Bolo <pair+iainkirkpatrick.jonathanbolo@devbootcamp.com>
2015-05-21 14:44:46 +1200

    initial implementation of view-controller

--- public/js/application.js
+++ public/js/application.js
@@ -3,5 +3,29 @@ $(document).ready(function() {
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready
+  game = new Game();
+
+  $("#new-game-button").click(function (e) {
+    game.new(updateView);
+  });
+
+  $("#roll-dice-button").click(function (e) {
+    game.dice.roll(updateView); //callback ?
+  });
+
+  function updateView(player, location, amount) {
+    updateMoney(player, amount);
+    movePlayer(location, player);
+  }
+
+  function updateMoney(player, amount) {
+    $("#money-player-" + player).html(amount);
+  }
+
+  function movePlayer(location, player)  {
+    $(".piece-player-" + player).removeClass(".piece-player-" + player);
+    $("#square-" + location).addClass(".piece-player-" + player);
+  }
+
 });
